8b3b77ff84782bed13fe806a01934e907de7f19e
TheHottestGoose <nicolemenzel@icloud.com>
2022-02-01 08:02:58 +0000

    changes_questions

--- run.py
+++ run.py
@@ -7,14 +7,10 @@ app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 
-def random_question():
-    with open("questions.json", 'r') as f:
-        questions = json.load(f)
-    return choice(questions)
 
 def next_question(q_id):
     with open("questions.json", 'r') as f:
-        questions = json.load(f)
+        questions = json.load()
     return choice(questions)
 
 def check_answer(q_id, a_id):
@@ -30,9 +26,10 @@ def home():
     return render_template("index.html")
 
 
-@app.route("/question")
-def question():
-    return render_template("question.html", question=random_question())
+
+@app.route("/next_question/<int:question_id>")
+def question_ordered(question_id):
+    return render_template("question.html", question=next_question(question_id+1))
 
 
 @app.route("/answer/<int:question_id>/<int:answer_id>")
